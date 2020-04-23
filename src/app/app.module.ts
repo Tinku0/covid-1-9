66c77f72a3a3f93c2e55d40b1bb4e4647da0dcbf
Tinku0 <varunchowdary23@gmail.com>
2020-04-23 15:38:17 +0530

    2 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HomeComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
